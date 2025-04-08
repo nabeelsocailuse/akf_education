898138b2bc850663d323f988a68f2a744ddc0b1c
githubammar <shaheenammar362@gmail.com>
2025-04-08 13:40:29 +0000

    visitor

--- akf_education/akf_education/doctype/visitor_management_system/visitor_management_system.js
+++ akf_education/akf_education/doctype/visitor_management_system/visitor_management_system.js
@@ -1,8 +1,38 @@
-// Copyright (c) 2025, Mubarrim Iqbal and contributors
-// For license information, please see license.txt
+frappe.ui.form.on('Visitor Management System', {
+    validate: function (frm) {
+        return new Promise((resolve, reject) => {
+            if (!frm.doc.cnicpassport_no) {
+                frappe.throw("Please enter CNIC/Passport number.");
+                reject();
+                return;
+            }
 
-// frappe.ui.form.on("Visitor Management System", {
-// 	refresh(frm) {
-
-// 	},
-// });
+            frappe.call({
+                method: "frappe.client.get_list",
+                args: {
+                    doctype: "Guardian",
+                    filters: {
+                        cnic_number: frm.doc.cnicpassport_no
+                    },
+                    limit_page_length: 1
+                },
+                callback: function (r) {
+                    if (r.message && r.message.length > 0) {
+                        frappe.show_alert({
+                            message:__('Guarian Found! '),
+                            indicator:'green'
+                        }, 5);
+                        resolve();
+                    } else {
+                        frappe.msgprint({
+                            title: "Guardian Status",
+                            message: "❌ Guardian not available. Cannot save this entry.",
+                            indicator: "red"
+                        });
+                        reject();
+                    }
+                }
+            });
+        });
+    }
+});

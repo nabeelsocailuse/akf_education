72d2339e390dfd8fc734c7aad9c112bcac2a8755
nabeelsocialuse <nabeel.social.use@gmail.com>
2025-06-13 12:18:41 +0000

    Personal Assessment Bug Fix - mubarrim

--- akf_education/akf_education/doctype/personal_assessment/personal_assessment.py
+++ akf_education/akf_education/doctype/personal_assessment/personal_assessment.py
@@ -6,27 +6,26 @@ from frappe.model.document import Document
 
 
 class PersonalAssessment(Document):
+	pass
 	# begin: auto-generated types
 	# This code is auto-generated. Do not modify anything in this block.
 
-	from typing import TYPE_CHECKING
+	# from typing import TYPE_CHECKING
 
-	if TYPE_CHECKING:
-		from frappe.types import DF
+	# if TYPE_CHECKING:
+	# 	from frappe.types import DF
 
-		completes_work_in_detail: DF.Literal["Strongly Disagree", "Somewhat Disagree", "Neutral", "Somewhat Agree", "Strongly Agree"]
-		date: DF.Date | None
-		generally_trustworthy: DF.Literal["Strongly Disagree", "Somewhat Disagree", "Neutral", "Somewhat Agree", "Strongly Agree"]
-		gets_nervous_quickly: DF.Literal["Strongly Disagree", "Somewhat Disagree", "Neutral", "Somewhat Agree", "Strongly Agree"]
-		has_less_creative_hobbies: DF.Literal["Strongly Disagree", "Somewhat Disagree", "Neutral", "Somewhat Agree", "Strongly Agree"]
-		i_find_faults_in_others_i_disagree: DF.Literal["Strongly Disagree", "Somewhat Disagree", "Neutral", "Somewhat Agree", "Strongly Agree"]
-		lives_within_oneself: DF.Literal["Strongly Disagree", "Somewhat Disagree", "Neutral", "Somewhat Agree", "Strongly Agree"]
-		name: DF.Int | None
-		outgoing_and_sociable: DF.Literal["Strongly Disagree", "Somewhat Disagree", "Neutral", "Somewhat Agree", "Strongly Agree"]
-		possesses_strong_ideas: DF.Literal["Strongly Disagree", "Somewhat Disagree", "Neutral", "Somewhat Agree", "Strongly Agree"]
-		registration_number: DF.Data
-		remains_calm_deals_well_with_anxiety: DF.Literal["Strongly Disagree", "Somewhat Disagree", "Neutral", "Somewhat Agree", "Strongly Agree"]
-		student_name: DF.Data | None
-		tends_towards_laziness: DF.Literal["Strongly Disagree", "Somewhat Disagree", "Neutral", "Somewhat Agree", "Strongly Agree"]
+	# 	completes_work_in_detail: DF.Literal["Strongly Disagree", "Somewhat Disagree", "Neutral", "Somewhat Agree", "Strongly Agree"]
+	# 	date: DF.Date | None
+	# 	generally_trustworthy: DF.Literal["Strongly Disagree", "Somewhat Disagree", "Neutral", "Somewhat Agree", "Strongly Agree"]
+	# 	gets_nervous_quickly: DF.Literal["Strongly Disagree", "Somewhat Disagree", "Neutral", "Somewhat Agree", "Strongly Agree"]
+	# 	has_less_creative_hobbies: DF.Literal["Strongly Disagree", "Somewhat Disagree", "Neutral", "Somewhat Agree", "Strongly Agree"]
+	# 	i_find_faults_in_others_i_disagree: DF.Literal["Strongly Disagree", "Somewhat Disagree", "Neutral", "Somewhat Agree", "Strongly Agree"]
+	# 	lives_within_oneself: DF.Literal["Strongly Disagree", "Somewhat Disagree", "Neutral", "Somewhat Agree", "Strongly Agree"]
+	# 	name: DF.Int | None
+	# 	outgoing_and_sociable: DF.Literal["Strongly Disagree", "Somewhat Disagree", "Neutral", "Somewhat Agree", "Strongly Agree"]
+	# 	possesses_strong_ideas: DF.Literal["Strongly Disagree", "Somewhat Disagree", "Neutral", "Somewhat Agree", "Strongly Agree"]
+	# 	remains_calm_deals_well_with_anxiety: DF.Literal["Strongly Disagree", "Somewhat Disagree", "Neutral", "Somewhat Agree", "Strongly Agree"]
+	# 	student_name: DF.Data | None
+	# 	tends_towards_laziness: DF.Literal["Strongly Disagree", "Somewhat Disagree", "Neutral", "Somewhat Agree", "Strongly Agree"]
 	# end: auto-generated types
-	pass

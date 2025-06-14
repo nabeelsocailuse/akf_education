1f2704a02470ec19cb247e5c0d7bad60bb7dafaa
nabeelsocialuse <nabeel.social.use@gmail.com>
2025-06-14 12:33:21 +0000

    Student Transfer - mubarrim

--- akf_education/akf_education/doctype/student_transfer/student_transfer.js
+++ akf_education/akf_education/doctype/student_transfer/student_transfer.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, Mubarrim Iqbal and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Shift Student", {
+// frappe.ui.form.on("Student Transfer", {
 // 	refresh(frm) {
 
 // 	},
--- akf_education/akf_education/doctype/student_transfer/test_student_transfer.py
+++ akf_education/akf_education/doctype/student_transfer/test_student_transfer.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestShiftStudent(FrappeTestCase):
+class TestStudentTransfer(FrappeTestCase):
 	pass

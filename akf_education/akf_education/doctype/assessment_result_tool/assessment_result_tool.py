57af91a80667494159a96127dbf4d8546facaca3
nabeelsocialuse <nabeel.social.use@gmail.com>
2025-06-17 10:27:52 +0000

    Assessment Result Tool - mubarrim

--- akf_education/akf_education/doctype/assessment_result_tool/assessment_result_tool.py
+++ akf_education/akf_education/doctype/assessment_result_tool/assessment_result_tool.py
@@ -1,12 +1,27 @@
 # Copyright (c) 2025, Mubarrim Iqbal and contributors
 # For license information, please see license.txt
-
 import frappe
+from frappe import _
 from frappe.model.document import Document
 
-
 class AssessmentResultTool(Document):
-	
+	def validate_marks(self):
+		for row in self.details:
+			if not row.score or not row.maximum_score:
+				frappe.throw(
+					_(f"Marks cannot be empty in row#{row.idx}. Please enter a valid marks value.")
+				)
+				
+			if row.score and row.maximum_score < 0:
+				frappe.throw(
+					_(f"Marks cannot be negative in row#{row.idx}. Please enter a valid marks value.")
+				)
+
+			if row.score and row.maximum_score > 100:
+				frappe.throw(
+					_(f"Marks cannot exceed 100 in row#{row.idx}. Please enter a valid marks value.")
+				)
+
 	@frappe.whitelist()
 	def get_latest_checked_enrollment(self):
 		prog_enrollment = frappe.get_all(
@@ -34,6 +49,7 @@ class AssessmentResultTool(Document):
 	
 	@frappe.whitelist()
 	def generate_assessment_result(self):
+		self.validate_marks()
 		if not self.current_program_enrollment:
 			frappe.throw("No program enrollment found for the student.")
 		

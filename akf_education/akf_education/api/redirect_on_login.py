1a5d6f15632adda688bd72f66cf3ceb08be5e3e4
nabeelsocialuse <nabeel.social.use@gmail.com>
2025-08-19 07:33:16 +0000

    removing console logs - mubarrim

--- akf_education/akf_education/api/redirect_on_login.py
+++ akf_education/akf_education/api/redirect_on_login.py
@@ -1,74 +1,11 @@
-# import frappe
-
-# def redirect_on_login(login_manager):
-#     # frappe.throw(f"hitting login")
-#     user = frappe.get_doc("User", login_manager.user)
-#     roles = [d.role for d in user.roles]
-
-#     # Change 'Sales User' to your role and '/app/sales-invoice' to your target page
-#     if "Manager Aghosh Homes" in roles:
-#         frappe.throw(f"hitting login")
-#         frappe.local.response["type"] = "redirect"
-#         frappe.local.response["location"] = "/app/executive-dashboard"
-
-
-# import frappe
-# from frappe.auth import LoginManager
-# from frappe import _
-
-# @frappe.whitelist(allow_guest=True)
-# def custom_login():
-#     # This replicates frappe's default login
-#     form_dict = frappe.local.form_dict
-#     login_manager = LoginManager()
-
-#     # Authenticate
-#     login_manager.authenticate(form_dict.get("usr"), form_dict.get("pwd"))
-#     login_manager.post_login()
-
-#     # Get roles for logged-in user
-#     user = frappe.get_doc("User", login_manager.user)
-#     roles = [d.role for d in user.roles]
-
-#     # Redirect based on role
-#     if "Manager Aghosh Homes" in roles:
-#         frappe.local.response["type"] = "redirect"
-#         frappe.local.response["location"] = "/app/executive-dashboard"
-#     else:
-#         # Default desk
-#         frappe.local.response["type"] = "redirect"
-#         frappe.local.response["location"] = "/app"
-
-#     return frappe.local.response
-
-
-# import frappe
-# from werkzeug.utils import redirect
-
-# def redirect_on_login(login_manager):
-#     user = frappe.get_doc("User", login_manager.user)
-#     roles = [d.role for d in user.roles]
-
-#     # Only redirect Desk users (not Portal / API logins)
-#     if frappe.local.request.path == "/login" and "Manager Aghosh Homes" in roles:
-#         frappe.local.flags.redirect_location = "/app/executive-dashboard"
-#         raise frappe.Redirect
-
 import frappe
 
 @frappe.whitelist()
 def reroute():
     user = frappe.session.user
     roles = frappe.get_roles(user)
+    
     if "Central Office Focal Person" in roles:
-        # frappe.msgprint(f"User: {user}, Roles: {roles}")
-    #     return "/app/executive-dashboard"
-    # else:
-    #     return "/app/home"
-        # frappe.local.response["type"] = "redirect"
-        # frappe.local.response["location"] = "/app/executive-dashboard"
-        # frappe.local.flags.redirect_location = "/app/executive-dashboard"
-        # raise frappe.Redirect
         frappe.local.response["home_page"] = "/app/executive-dashboard"
-
- 
+    elif "Manager Aghosh Homes" or "Aghosh Administrator" in roles:
+        frappe.local.response["home_page"] = "/app/aghosh-home-details"
--- akf_education/akf_education/doctype/incident_report/incident_report.js
+++ akf_education/akf_education/doctype/incident_report/incident_report.js
@@ -1,10 +1,21 @@
 // Copyright (c) 2025, Mubarrim Iqbal and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Incident Report", {
-// 	onload: function(frm) {
-//         if (!frm.doc.closure_date) {
-//             frm.set_value('closure_date', frappe.datetime.now_datetime());
-//         }
-//     }
-// });
+frappe.ui.form.on("Incident Report", {
+    refresh: function (frm) {
+        frm.set_query("affected_person", function () {
+            return {
+                filters: {
+                    "status": 'Active',
+                },
+            };
+
+        });
+
+        // 	onload: function(frm) {
+        //         if (!frm.doc.closure_date) {
+        //             frm.set_value('closure_date', frappe.datetime.now_datetime());
+        //         }
+        //     }
+    }
+});
